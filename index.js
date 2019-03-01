20bd6fbe0e84b1390226e7f5d63b710bfb3aa737
Scott Williams <scottcolemanwilliams@gmail.com>
2019-03-01 14:46:16 -0600

    finished simple GET requests 2

--- index.js
+++ index.js
@@ -5,16 +5,26 @@ const store = {
   dogs: [],
 };
 
+function renderDogs() {
+  $('.container').html(store.dogs.map(e => `<img src="${e}" alt="a dog">`));
+}
+
 function getDogImage() {
-  fetch(`https://dog.ceo/api/breeds/image/random/${$('#num-dogs').val()}`)
+  return fetch(`https://dog.ceo/api/breeds/image/random/${$('#num-dogs').val()}`)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(dogImage => {
+      store.dogs = dogImage.message;
+      //renderDogs();
+      store.dogs.forEach(dog => console.log(dog));
+      return;
+    });
+
 }
 
 function watchForm() {
   $('#dogform').submit(event => {
     event.preventDefault();
-    getDogImage();  
+    getDogImage().then(renderDogs);
   });
 }
 
